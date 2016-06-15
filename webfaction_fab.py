7a5aa3066b592a0781b51d39ee0116d45f94d241
Martin Massera <martinmassera@gmail.com>
2016-06-15 00:55:32 -0300

    better fabfile

--- webfaction_fab.py
+++ webfaction_fab.py
@@ -4,16 +4,26 @@ import fabfile_settings
 
 @task
 def prod():
+    print 'PRODUCTION environment'
     env.hosts = [fabfile_settings.PROD_SERVER]
     env.remote_app_dir = os.path.join(fabfile_settings.APP_DIR, fabfile_settings.APP_NAME)
     env.remote_apache_dir = os.path.join(fabfile_settings.APP_DIR, 'apache2')
     env.venv_app = fabfile_settings.VENV_SCRIPT
 
 @task
+def test():
+    print 'TEST environment'
+    env.hosts = [fabfile_settings.PROD_SERVER]
+    env.remote_app_dir = os.path.join(fabfile_settings.APP_DIR_TEST, fabfile_settings.APP_NAME)
+    env.remote_apache_dir = os.path.join(fabfile_settings.APP_DIR_TEST, 'apache2')
+    env.venv_app = fabfile_settings.VENV_SCRIPT_TEST
+
+
+@task
 def pip():
     '''install requirements'''
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         run("pip install -r requirements.txt")
 
@@ -26,8 +36,8 @@ def commit():
 @task
 def push():
     '''push and pull'''
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     local("git push origin master")
     with prefix(env.venv_app):
         run("git pull")
@@ -35,32 +45,32 @@ def push():
 @task
 def collectstatic():
     '''collect static files'''
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         run("python manage.py collectstatic --noinput")
 
 @task
 def migrate():
     '''execute migrations'''
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         run("python manage.py migrate")
 
 @task
 def restart():
     """Restart apache on the server."""
-    require('hosts', provided_by=[prod])
-    require('remote_apache_dir', provided_by=[prod])
+    require('hosts')
+    require('remote_apache_dir')
     run("%s/bin/restart;" % (env.remote_apache_dir))
 
 @task
 def deploy():
     '''push, pull, collect static, restart'''
-    require('hosts', provided_by=[prod])
-    require('remote_app_dir', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('remote_app_dir')
+    require('venv_app')
     push()
     collectstatic()
     migrate()
