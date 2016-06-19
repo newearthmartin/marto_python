27581a0831b0606ef154bf657930d4ee77eec48a
Martin Massera <martinmassera@gmail.com>
2016-06-19 00:36:29 -0300

    runjobs

--- email/jobs/send_emails.py
+++ email/jobs/send_emails.py
@@ -1,13 +1,15 @@
 import logging
+
 logger = logging.getLogger(__name__)
 
-from django.core.management.base import BaseCommand, CommandError
+from django_extensions.management.jobs import HourlyJob
 
 from marto_python.email.backend import DBEmailBackend
 
-class Command(BaseCommand):
-    help = 'tells db backend to send emails'
 
-    def handle(self, *args, **options):
+class Job(HourlyJob):
+    help = "try to send all queued emails"
+
+    def execute(self):
         logger.info('Telling db backend to send emails...')
         DBEmailBackend().send_all()
