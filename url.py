7e5745f002306563a76c4417eb552dfa78e8a46a
Martin massera <martinmassera@gmail.com>
2017-03-20 02:20:29 -0300

    refactoring

--- url.py
+++ url.py
@@ -20,3 +20,24 @@ def urldecode(string):
 
 def is_absolute(url):
     return bool(urlparse.urlparse(url).netloc)
+
+def get_param(request, param_name, empty_valid=False, default=None, encode_unicode=True):
+    val = default
+    if param_name in request.GET:
+        tempVal = request.GET[param_name]
+        if empty_valid or len(tempVal) > 0:
+            val = tempVal
+    if encode_unicode and val is not None:
+        val = unicode(val)
+    return val
+
+def post_param(request, param_name, empty_valid=False, default=None, encode_unicode=True):
+    val = default
+    if param_name in request.POST:
+        tempVal = request.POST[param_name]
+        if empty_valid or len(tempVal) > 0:
+            val = tempVal
+    if encode_unicode and val is not None:
+        val = unicode(val)
+    return val
+
