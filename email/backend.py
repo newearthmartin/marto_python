2331e1dd269f599d45e8c7e6468d77ceee699e98
Martín Massera <martinmassera@gmail.com>
2020-10-02 14:23:17 -0300

    new changes

--- email/backend.py
+++ email/backend.py
@@ -1,28 +1,41 @@
 # encoding: utf-8
-import email
+import json
+import datetime
+import smtplib
 import logging
-logger = logging.getLogger(__name__)
 
+from django.conf import settings
+from django.utils import timezone
 from django.core.mail.backends.base import BaseEmailBackend
 
 from marto_python.email.models import EmailMessage
-from marto_python.util import list2comma_separated, load_class, setting
+from marto_python.util import get_full_class, load_class, setting
+from marto_python.collections import list2comma_separated
+
+logger = logging.getLogger(__name__)
+
 
 class DecoratorBackend(BaseEmailBackend):
-    '''abstract class for decorators to add functionality to EmailBackend in a decorator pattern'''      
+    """abstract class for decorators to add functionality to EmailBackend in a decorator pattern"""
     inner_backend = None
 
-    def __init__(self, inner_backend_settings_property):
-        class_name = setting(inner_backend_settings_property)
-        if class_name:
-            self.setInnerBackend(load_class(class_name)())
+    def __init__(self, *args, **kwargs):
+        super(DecoratorBackend, self).__init__(*args, **kwargs)
+        backend_instance = kwargs.get('inner_backend')
+        backend_class = kwargs.get('inner_backend_class')
+        if backend_instance: self.set_inner_backend(backend_instance)
+        elif backend_class: self.set_inner_backend_class(backend_class)
 
-    def setInnerBackend(self, backend):
+    def set_inner_backend(self, backend):
         self.inner_backend = backend
+
+    def set_inner_backend_class(self, backend_class):
+        self.set_inner_backend(load_class(backend_class)())
 
     def send_messages(self, email_messages):
         if self.inner_backend:
             self.inner_backend.send_messages(email_messages)
+
     def open(self):
         if self.inner_backend:
             self.inner_backend.open()
@@ -31,22 +44,139 @@ class DecoratorBackend(BaseEmailBackend):
         if self.inner_backend:
             self.inner_backend.close()
 
+
+class StackedEmailBackend(DecoratorBackend):
+    def __init__(self, *args, **kwargs):
+        inner_backend = None
+        for backend_class in settings.EMAIL_BACKEND_STACK:
+            inner_backend = load_class(backend_class)(*args, inner_backend=inner_backend, **kwargs)
+        super(StackedEmailBackend, self).__init__(*args, inner_backend=inner_backend, **kwargs)
+
+
 class DBEmailBackend(DecoratorBackend):
     def __init__(self, *args, **kwargs):
-        super(DBEmailBackend, self).__init__('EMAIL_DB_BACKEND_INNER_BACKEND')
+        super(DBEmailBackend, self).__init__(*args, **kwargs)
+        if not self.inner_backend:
+            class_name = getattr(settings,
+                                 'EMAIL_DB_BACKEND_INNER_BACKEND',
+                                 'django.core.mail.backends.smtp.EmailBackend')
+            if class_name:
+                self.set_inner_backend_class(class_name)
+
+    @staticmethod
+    def django_message_to_db_email(message):
+        connection = message.connection
+        message.connection = None
+        dump = json.dumps(message.__dict__)
+        message.connection = connection
+        email = EmailMessage()
+        email.from_email =  message.from_email
+        email.subject =     message.subject
+        email.body =        message.body
+        email.to =          list2comma_separated(message.to)
+        email.cc =          list2comma_separated(message.cc)
+        email.bcc =         list2comma_separated(message.bcc)
+        email.email_class = get_full_class(message)
+        email.email_dump =  dump
+        return email
+
+    @staticmethod
+    def db_email_to_django_message(email):
+        message = load_class(email.email_class)()
+        message.__dict__ = json.loads(email.email_dump)
+        return message
+
     def send_messages(self, email_messages):
-        for message in email_messages:
-            email = EmailMessage()
-            email.sender    = message.from_email
-            email.subject   = message.subject
-            email.body      = message.body
-            email.to        = list2comma_separated(message.to)
-            email.cc        = list2comma_separated(message.cc)
-            email.bcc       = list2comma_separated(message.bcc)
-            #attachments: A list of attachments to put on the message. These can be either email.MIMEBase.MIMEBase instances, or (filename, content, mimetype) triples.
-            #headers: A dictionary of extra headers to put on the message. The keys are the header name, values are the header values. It’s up to the caller to ensure header names and values are in the correct format for an email message. The corresponding attribute is extra_headers.
+        db_emails = map(DBEmailBackend.django_message_to_db_email, email_messages)
+        for email in db_emails:
             email.save()
-        super(DBEmailBackend, self).send_messages(email_messages)
+        if getattr(settings, "EMAIL_DB_BACKEND_SEND_IMMEDIATELY", False):
+            logger.debug('sending emails now')
+            self.send_emails(db_emails)
+        else:
+            logger.info('stored %d emails for sending later' % len(db_emails)) # PY3: fixme
+
+    def send_all(self):
+        self.send_queryset(EmailMessage.objects)
+
+    def send_queryset(self, emails_queryset):
+        """
+        sends all emails in the queryset that haven't been sent
+        """
+        self.send_emails(emails_queryset.filter(sent=False).all())
+
+    def send_emails(self, emails):
+        """
+        sends all db emails in list.
+        """
+        max_today = getattr(settings, 'EMAIL_DB_BACKEND_MAX_DAILY_TOTAL', 2000)
+        max_by_subject = getattr(settings, 'EMAIL_DB_BACKEND_MAX_DAILY_BY_SUBJECT', 700)
+
+        yesterday24hs = timezone.now() - datetime.timedelta(days=1)
+        emails_sent_today = EmailMessage.objects.filter(sent=True).filter(sent_on__gt=yesterday24hs)
+        num_emails_sent_today = emails_sent_today.count()
+        total_allowed_emails = max_today - num_emails_sent_today
+        logger.debug('Sending emails - already sent %d emails - can send %d more'
+                    % (num_emails_sent_today, total_allowed_emails))
+        if total_allowed_emails < 0:
+            logger.error('Sent %d emails but only %d were allowed!' % (num_emails_sent_today, max_today))
+            return
+
+        allowed_emails = []
+        subjects = {}
+        for email in emails:
+            if len(allowed_emails) >= total_allowed_emails:
+                logger.warning('reached maximum total emails')
+                break
+            subject = email.subject
+            count = subjects[subject] if subject in subjects else emails_sent_today.filter(subject=subject).count()
+            logger.debug('Already sent %d (maximum %d) for subject \'%s\'' % (count, max_by_subject, subject))
+            if count >= max_by_subject:
+                continue
+            count += 1
+            subjects[subject] = count
+            logger.info('sending email - subject: %s' % subject)
+            logger.debug('sending email - ok for sending - updated count %d - maximum %d' % (count, max_by_subject))
+            allowed_emails.append(email)
+
+        if allowed_emails: self.do_send(allowed_emails)
+
+    def do_send(self, emails):
+        logger.info('sending %d emails' % len(emails))
+        for email in emails:
+            # check again if its not sent, for concurrency
+            if email.sent:
+                logger.debug('email already sent %s - %s' % (email.to, email.subject))
+                continue
+            email_message = DBEmailBackend.db_email_to_django_message(email)
+            email.sent = False
+            email.send_successful = False
+            try:
+                super(DBEmailBackend, self).send_messages([email_message])
+                email.send_successful = True
+                email.sent = True
+            except (smtplib.SMTPDataError, smtplib.SMTPRecipientsRefused) as e:
+                email.fail_message = unicode(e)
+                logger.warn('error sending email to %s' % email.to, exc_info=True)
+                email.sent = True
+            except TypeError as e:
+                email.fail_message = unicode(e)
+                logger.warn('error sending email to %s' % email.to, exc_info=True)
+                email.sent = True
+            except smtplib.SMTPConnectError:
+                logger.warn('error sending email to %s' % email.to, exc_info=True)
+            except:
+                msg = 'unknown exception sending email to %s' % email.to
+                has_admin_emails = [e for e in settings.ADMINS if e[1].lower() == email.to.lower()]
+                if has_admin_emails:
+                    logger.warn(msg, exc_info=True)
+                else:
+                    logger.error(msg, exc_info=True)
+            if email.sent:
+                email.sent_on = timezone.now()
+                email.save()
+        logger.debug('sending %d emails - finished' % len(emails))
+
 
 class FilteringEmailBackend(DecoratorBackend):
     filter = True
@@ -54,10 +184,11 @@ class FilteringEmailBackend(DecoratorBackend):
     redirect_to = []
 
     def __init__(self, *args, **kwargs):
-        super(FilteringEmailBackend, self).__init__('EMAIL_FILTERING_BACKEND_INNER_BACKEND')
+        class_name = getattr(settings, 'EMAIL_FILTERING_BACKEND_INNER_BACKEND')
+        super(FilteringEmailBackend, self).__init__(*args, inner_backend_class=class_name, **kwargs)
         self.filter = setting('EMAIL_FILTERING_BACKEND_FILTER', default=True)
-        self.pass_emails     = setting('EMAIL_FILTERING_BACKEND_PASS_EMAILS'    , default=[])
-        self.redirect_to     = setting('EMAIL_FILTERING_BACKEND_REDIRECT_TO'    , default=[])
+        self.pass_emails = setting('EMAIL_FILTERING_BACKEND_PASS_EMAILS', default=[])
+        self.redirect_to = setting('EMAIL_FILTERING_BACKEND_REDIRECT_TO', default=[])
 
     def send_messages(self, email_messages):
         for message in email_messages:
@@ -67,8 +198,7 @@ class FilteringEmailBackend(DecoratorBackend):
                 for address in all_recipients:
                     if address not in self.pass_emails:
                         send = False
-            status = ''
-            message_string = '%s (to:%s cc:%s bcc:%s)' %  (message.subject,
+            message_string = '%s (to:%s cc:%s bcc:%s)' % (message.subject,
                                                           list2comma_separated(message.to),
                                                           list2comma_separated(message.cc),
                                                           list2comma_separated(message.bcc))
--- email/models.py
+++ email/models.py
@@ -1,49 +1,49 @@
 import random
-from django import forms
 from django.db import models
 from django.utils.crypto import get_random_string
-from django.contrib.admin.options import ModelAdmin
-from django.contrib.admin.widgets import AdminTextInputWidget
-from marto_python.email import send_email
-from tinymce.widgets import TinyMCE
+from marto_python.email.email import send_email
 
 random.seed()
 
-# Create your models here.
+
 class EmailMessage(models.Model):
-    from_email  = models.EmailField(null=False, blank=False)
-    to          = models.TextField(null=True, blank=True) #comma separated list of recipients
-    cc          = models.TextField(null=True, blank=True) #comma separated list of recipients
-    bcc         = models.TextField(null=True, blank=True) #comma separated list of recipients
-    subject     = models.CharField(max_length=255, null=True, blank=True)
-    body        = models.TextField(null=True, blank=True)
-    timestamp   = models.DateTimeField(null=False, auto_now_add=True)
+    from_email = models.CharField(max_length=512, null=False, blank=False)
+    to = models.TextField(null=True, blank=True)  # comma separated list of recipients
+    cc = models.TextField(null=True, blank=True)  # comma separated list of recipients
+    bcc = models.TextField(null=True, blank=True)  # comma separated list of recipients
+    subject = models.CharField(max_length=512, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    sent = models.BooleanField(default=False, db_index=True)
+    sent_on = models.DateTimeField(null=True, blank=True, db_index=True)
+    send_successful = models.BooleanField(default=False)
+    fail_message = models.CharField(max_length=256, null=True, blank=True)
+    email_class = models.CharField(max_length=256)
+    email_dump = models.TextField()
+
     def __unicode__(self):
         return self.subject
-    class AdminForm(forms.ModelForm):
-        class Meta:
-            widgets = {
-                'to': AdminTextInputWidget,
-                'cc': AdminTextInputWidget,
-                'bcc': AdminTextInputWidget,
-                'body': TinyMCE(attrs={'cols': 120, 'rows': 50}),
-            }
-class EmailMessageAdmin(ModelAdmin):
-    form = EmailMessage.AdminForm
-    list_display = ['to', 'subject', 'timestamp']
-    list_filter = ['to', 'subject']
-    search_fields = ['from_email', 'to', 'cc', 'bcc', 'subject', 'body']
 
-#for mixing into the UserProfile model
+    def clear_sent(self):
+        self.sent = False
+        self.send_successful = False
+        self.fail_message = None
+        self.sent_on = None
+        self.save()
+
+
 class EmailConfirmationMixin(models.Model):
+    """ For mixing into the UserProfile model """
     class Meta:
         abstract = True
-    email_confirmed             = models.BooleanField(blank=False, null=False, default=False, verbose_name='email confirmed')
-    email_confirmation_key      = models.CharField(max_length=40, blank=True, null=True, default=None, verbose_name='email confirmation key' )
 
-    #users should override this method if user is different from "self.user"
+    email_confirmed = models.BooleanField(blank=False, null=False, default=False, verbose_name='email confirmed')
+    email_confirmation_key = models.CharField(max_length=100, blank=True, null=True, default=None, verbose_name='email confirmation key')
+
+    # users should override this method if user is different from "self.user"
     def get_user(self):
         return self.user
+
     def get_primary_email(self):
         return self.get_user().email
 
@@ -57,12 +57,12 @@ class EmailConfirmationMixin(models.Model):
             self.save(update_fields=['email_confirmed', 'email_confirmation_key'])
         else:
             self.generate_confirmation_key()
-            
+
     def generate_confirmation_key(self):
         self.email_confirmation_key = get_random_string()
         self.email_confirmed = False
         self.save(update_fields=['email_confirmed', 'email_confirmation_key'])
-        
+
     def confirm_email(self, key):
         if key == self.email_confirmation_key:
             self.email_confirmed = True
@@ -71,12 +71,11 @@ class EmailConfirmationMixin(models.Model):
             return True
         else:
             return False
-    #does not generate key
-    def send_email_confirmation(self, subject, template, context=None):
+
+    def send_email_confirmation(self, subject, template, context=None):  # does not generate key
         if context is None:
             context = {}
         user = self.get_user()
         context['user'] = user
         context['email_confirmation'] = self
         send_email(user.email, subject, template, context)
-
