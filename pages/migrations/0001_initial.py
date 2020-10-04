9d5b3cf1753bf1198591a5a98e1a87593990d066
Martin Massera <martinmassera@gmail.com>
2020-10-04 00:50:20 +0000

    reset migrations for 1.7

--- pages/migrations/0001_initial.py
+++ pages/migrations/0001_initial.py
@@ -1,63 +1,50 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Menu'
-        db.create_table(u'pages_menu', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('titulo', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('padre', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='children', null=True, to=orm['pages.Menu'])),
-            ('indice', self.gf('django.db.models.fields.IntegerField')(default=0)),
-            ('pagina', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='menu', null=True, to=orm['pages.Pagina'])),
-            ('url', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-            ('totalUrl', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-        ))
-        db.send_create_signal(u'pages', ['Menu'])
+    dependencies = [
+    ]
 
-        # Adding model 'Pagina'
-        db.create_table(u'pages_pagina', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('url', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('titulo', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('contenido', self.gf('django.db.models.fields.TextField')()),
-            ('extra_css', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-        ))
-        db.send_create_signal(u'pages', ['Pagina'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Menu'
-        db.delete_table(u'pages_menu')
-
-        # Deleting model 'Pagina'
-        db.delete_table(u'pages_pagina')
-
-
-    models = {
-        u'pages.menu': {
-            'Meta': {'object_name': 'Menu'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'indice': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'padre': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['pages.Menu']"}),
-            'pagina': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'menu'", 'null': 'True', 'to': u"orm['pages.Pagina']"}),
-            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'totalUrl': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
-        },
-        u'pages.pagina': {
-            'Meta': {'object_name': 'Pagina'},
-            'contenido': ('django.db.models.fields.TextField', [], {}),
-            'extra_css': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'url': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['pages']
+    operations = [
+        migrations.CreateModel(
+            name='Menu',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('titulo', models.CharField(max_length=255)),
+                ('seccion', models.CharField(max_length=10, choices=[(b'PR', 'men\xfa principal'), (b'FO', 'pie de p\xe1gina')])),
+                ('indice', models.IntegerField(default=0)),
+                ('url', models.CharField(max_length=255, null=True, blank=True)),
+                ('total_url', models.CharField(max_length=255, null=True, blank=True)),
+                ('padre', models.ForeignKey(related_name=b'children', blank=True, to='pages.Menu', null=True)),
+            ],
+            options={
+                'verbose_name': 'men\xfa',
+                'verbose_name_plural': 'men\xfas',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Pagina',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('url', models.CharField(unique=True, max_length=255)),
+                ('titulo', models.CharField(max_length=255)),
+                ('contenido', models.TextField()),
+                ('extra_css', models.CharField(max_length=255, null=True, blank=True)),
+            ],
+            options={
+                'verbose_name': 'p\xe1gina',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='menu',
+            name='pagina',
+            field=models.ForeignKey(blank=True, to='pages.Pagina', null=True),
+            preserve_default=True,
+        ),
+    ]
