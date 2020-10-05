01e03f1c79d9685b45cf815ea3d854b7d69b1c21
Martín Massera <martinmassera@gmail.com>
2020-10-05 14:36:11 -0300

    migrations

--- email/migrations/0001_initial.py
+++ email/migrations/0001_initial.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import models, migrations
 
 
-# noinspection SpellCheckingInspection
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,19 +14,13 @@ class Migration(migrations.Migration):
             name='EmailMessage',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('from_email', models.CharField(max_length=256)),
+                ('from_email', models.EmailField(max_length=254)),
                 ('to', models.TextField(null=True, blank=True)),
                 ('cc', models.TextField(null=True, blank=True)),
                 ('bcc', models.TextField(null=True, blank=True)),
-                ('subject', models.CharField(max_length=256, null=True, blank=True)),
+                ('subject', models.CharField(max_length=255, null=True, blank=True)),
                 ('body', models.TextField(null=True, blank=True)),
-                ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('sent', models.BooleanField(default=False)),
-                ('sent_on', models.DateTimeField(null=True, blank=True)),
-                ('send_succesful', models.BooleanField(default=False)),
-                ('fail_message', models.CharField(max_length=256, null=True, blank=True)),
-                ('email_class', models.CharField(max_length=256)),
-                ('email_dump', models.TextField()),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
             ],
         ),
     ]
