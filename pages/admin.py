730b75df356b7aebd236abb85f20863aaf10451a
Martin Massera <martinmassera@gmail.com>
2020-10-05 22:10:00 -0300

    removing pages;

--- pages/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from marto_python.pages.models import Menu, Pagina, PaginaAdmin
-
-admin.site.register(Menu,   Menu.Admin)
-admin.site.register(Pagina, PaginaAdmin)
