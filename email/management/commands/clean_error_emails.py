0e9f32b367654c8c95351968bdc908efb6995ea4
Martin Massera <martinmassera@gmail.com>
2021-01-04 12:20:09 -0300

    cleaning also CRITICAL error messages

--- email/management/commands/clean_error_emails.py
+++ email/management/commands/clean_error_emails.py
@@ -1,12 +1,14 @@
 from django.core.management.base import BaseCommand
 from marto_python.email.models import EmailMessage
-
+from django.db.models import Q
 
 class Command(BaseCommand):
     help = 'cleans email database'
 
     def handle(self, *args, **options):
-        msgs = EmailMessage.objects.filter(subject__contains='[Django] ERROR').all()
-        print(f'There are {len(msgs)} django error messages, cleaning...')
-        for msg in msgs:
+        msgs = EmailMessage.objects.filter(
+            Q(subject__contains='[Django] ERROR') |
+            Q(subject__contains='[Django] CRITICAL'))
+        print(f'There are {msgs.count()} django error messages, cleaning...')
+        for msg in msgs.all():
             msg.delete()
