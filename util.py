a97daea1f6cde04c8e3214b08af0e1f9170197e1
Martin Massera <martinmassera@gmail.com>
2021-12-14 14:49:18 -0300

    pdf.py

--- util.py
+++ util.py
@@ -1,6 +1,5 @@
 import logging
 import math
-import pdfkit
 import datetime
 import importlib
 
@@ -152,22 +151,3 @@ def compose(f, g):
     def fog(*args, **kwargs):
         return f(g(*args, **kwargs))
     return fog
-
-
-def render_to_pdf(html, pdf_out_file, wkhtmltopdf_options=None):
-    wkhtmltopdf_path = settings.WKHTMLTOPDF_PATH
-    options = {
-        'page-size': 'A4',
-        'margin-top': '0.75in',
-        'margin-right': '0.75in',
-        'margin-bottom': '0.75in',
-        'margin-left': '0.75in',
-    }
-    if wkhtmltopdf_options:
-        options.update(wkhtmltopdf_options)
-    pdfkit.from_string(
-        html,
-        pdf_out_file,
-        configuration=pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path) if wkhtmltopdf_path else None,
-        options=options
-    )
