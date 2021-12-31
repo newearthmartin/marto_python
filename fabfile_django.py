8ebf65920cb34fabb6359f39f26fb6d05ee66114
Martin Massera <martinmassera@gmail.com>
2021-12-31 17:34:49 -0300

    simplifying fabfile

--- fabfile_django.py
+++ fabfile_django.py
@@ -17,7 +17,6 @@ def prod():
     env.webapp_dir = fab_settings['APP_DIR']
     env.restart_script = fab_settings['RESTART_SCRIPT']
     env.venv_app = fab_settings['VENV_SCRIPT']
-    print(f'{env.environment_name} environment')
 
 
 ################ GIT ################
@@ -35,8 +34,8 @@ def push():
     """
     push and pull
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     # local("git submodule foreach git push")
     local("git push origin master")
     with prefix(env.venv_app):
@@ -52,8 +51,8 @@ def pip():
     """
     install requirements
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         run("pip install -r requirements.txt")
 
@@ -63,8 +62,8 @@ def collectstatic():
     """
     collect static files
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         run("python manage.py collectstatic --noinput")
 
@@ -74,8 +73,8 @@ def migrate():
     """
     execute migrations
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         migrate_apps = fab_settings['MIGRATE_APPS']
         if not migrate_apps: migrate_apps = ''
@@ -87,8 +86,8 @@ def requirements():
     """
     install pip requirements
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         run("pip install -r requirements.txt")
 
@@ -98,8 +97,8 @@ def upgrade_pip():
     """
     upgrade pip
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         run("pip install --upgrade pip")
 
@@ -109,8 +108,8 @@ def restart():
     """
     Restart apache on the server.
     """
-    require('hosts', provided_by=[prod])
-    require('restart_script', provided_by=[prod])
+    require('hosts')
+    require('restart_script')
     with cd(env.webapp_dir):
         run(env.restart_script)
 
@@ -120,16 +119,16 @@ def deploy():
     """
     push, pull, collect static, restart
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     push()
     deploy_django()
 
 
 @task
 def deploy_django():
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     collectstatic()
     migrate()
     restart()
@@ -152,8 +151,8 @@ def db_dump():
     """
     dump entire db on server and retrieve it
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         run('mkdir -p data')
         run(f'./manage.py dumpdata {fab_settings["DUMP_DATA_MODELS"]} --indent=4 > data/db.json')
@@ -182,8 +181,8 @@ def initial_dump():
     """
     dump initial data on server and retrieve it
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     dump_initial_models = fab_settings["DUMP_INITIAL"]
     with prefix(env.venv_app):
         run(f'./manage.py dumpdata {dump_initial_models} --indent=4 > data/initial.json')
@@ -220,7 +219,7 @@ def hourly():
     """
     run hourly jobs, including submit mails
     """
-    require('venv_app', provided_by=[prod])
+    require('venv_app')
     with prefix(env.venv_app):
         run("python manage.py runjobs hourly")
 
@@ -233,14 +232,14 @@ def celery():
     """
     restarts celery
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     run('monit restart celery')
 
 
 @task
 def monit():
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     run("monit")
     run("monit status")
