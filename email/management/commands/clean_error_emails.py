0f41d388912038631904d876ab7b793c101b887f
Martin Massera <martinmassera@gmail.com>
2022-07-02 13:43:48 -0300

    admin filter for error emails

--- email/management/commands/clean_error_emails.py
+++ email/management/commands/clean_error_emails.py
@@ -2,14 +2,17 @@ from django.core.management.base import BaseCommand
 from marto_python.email.models import EmailMessage
 from django.db.models import Q
 
+
+def filter_error_emails(queryset, filter_not_exclude=True):
+    query = Q(subject__contains='[Django] ERROR') | Q(subject__contains='[Django] CRITICAL')
+    return queryset.filter(query) if filter_not_exclude else queryset.exclude(query)
+
+
 class Command(BaseCommand):
     help = 'cleans email database'
 
     def handle(self, *args, **options):
-        msgs = EmailMessage.objects.filter(
-            Q(subject__contains='[Django] ERROR') |
-            Q(subject__contains='[Django] CRITICAL'))
-
+        msgs = filter_error_emails(EmailMessage.objects)
         if msgs.count() > 0:
             print(f'There are {msgs.count()} django error messages, cleaning:')
             print()
@@ -18,4 +21,3 @@ class Command(BaseCommand):
                 msg.delete()
         else:
             print(f'There are 0 django error messages.')
-
