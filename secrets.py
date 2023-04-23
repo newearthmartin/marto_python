9bf3c1c1e6ab63b84578e6c533e444dcd5974803
Martin Massera <martinmassera@gmail.com>
2023-04-22 23:05:42 -0300

    simplifying read_secrets

--- secrets.py
+++ secrets.py
@@ -6,11 +6,9 @@ secrets = None
 
 
 def read_secrets(base_dir=None, secrets_file=None):
-    if not base_dir:
-        base_dir = '.'
     if not secrets_file:
         secrets_file = 'secrets.toml'
-    secrets_path = os.path.join(base_dir, secrets_file)
+    secrets_path = os.path.join(base_dir, secrets_file) if base_dir else secrets_file
     if os.path.exists(secrets_path):
         with open(secrets_path) as secrets_file:
             global secrets
