c5d401d6e40775cd6f4c738873aaba8d10519220
Martin Massera <martinmassera@gmail.com>
2023-05-22 23:15:25 +0000

    passing installed apps and middleware as arguments

--- debug.py
+++ debug.py
@@ -4,14 +4,15 @@ from django.urls import path, include
 use_debug_toolbar = False
 
 
-def setup_debug_toolbar():
+def setup_debug_toolbar(installed_apps, middleware):
     global use_debug_toolbar
     use_debug_toolbar = True
-    settings.INSTALLED_APPS.append('debug_toolbar')
-    settings.MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+    installed_apps.append('debug_toolbar')
+    middleware.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
 
 def add_debug_toolbar_if_enabled(urlpatterns):
     if use_debug_toolbar:
         import debug_toolbar
         urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
