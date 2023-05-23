8a4abd3ac7c4fbc7446fb751085d3d44b4bc49d0
Martin Massera <martinmassera@gmail.com>
2023-05-23 14:24:32 -0300

    rename methods

--- debug.py
+++ debug.py
@@ -1,18 +1,17 @@
-from django.conf import settings
 from django.urls import path, include
 
-use_debug_toolbar = False
+debug_toolbar_enabled = False
 
 
-def setup_debug_toolbar(installed_apps, middleware):
-    global use_debug_toolbar
-    use_debug_toolbar = True
+def enable_debug_toolbar(installed_apps, middleware):
+    global debug_toolbar_enabled
+    debug_toolbar_enabled = True
     installed_apps.append('debug_toolbar')
     middleware.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
 
 def add_debug_toolbar_if_enabled(urlpatterns):
-    if use_debug_toolbar:
+    if debug_toolbar_enabled:
         import debug_toolbar
         urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
 
