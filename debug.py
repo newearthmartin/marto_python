69547fa6bf13d69c5f821699839ac8089c96c9f2
Martin Massera <martinmassera@gmail.com>
2023-05-24 12:51:26 -0300

    better form for debug_toolbar

--- debug.py
+++ debug.py
@@ -1,17 +1,11 @@
 from django.urls import path, include
 
-debug_toolbar_enabled = False
 
-
-def enable_debug_toolbar(installed_apps, middleware):
-    global debug_toolbar_enabled
-    debug_toolbar_enabled = True
+def configure_debug_toolbar(installed_apps, middleware):
     installed_apps.append('debug_toolbar')
     middleware.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
 
-def add_debug_toolbar_if_enabled(urlpatterns):
-    if debug_toolbar_enabled:
-        import debug_toolbar
-        urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
-
+def add_debug_toolbar_urls(urlpatterns):
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
