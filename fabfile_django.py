cd2ebf0cc635701b2b7f1aa80d0f024e8e8bf499
Martin Massera <martinmassera@gmail.com>
2023-06-12 12:09:40 -0300

    fabfile_django from master

--- fabfile_django.py
+++ fabfile_django.py
@@ -4,7 +4,10 @@ from fabfile_settings import fab_settings
 
 
 def get_app_ssh_path():
-    return f"{fab_settings['PROD_SERVER']}:{fab_settings['APP_DIR']}/{fab_settings['APP_DJANGO_DIR']}"
+    server = fab_settings['PROD_SERVER']
+    app_dir = fab_settings['APP_DIR']
+    app_django_dir = fab_settings['APP_DJANGO_DIR']
+    return f'{server}:{app_dir}/{app_django_dir}'
 
 
 ################ ENVIRONMENT ################
@@ -16,8 +19,9 @@ def prod():
     env.hosts = [fab_settings['PROD_SERVER']]
     env.webapp_dir = fab_settings['APP_DIR']
     env.restart_script = fab_settings['RESTART_SCRIPT']
-    env.venv_app = fab_settings['VENV_SCRIPT']
-    print(f'{env.environment_name} environment')
+    env.venv_app = fab_settings.get('VENV_SCRIPT', f'cd {env.webapp_dir}')
+    env.main_branch = fab_settings.get('MAIN_BRANCH', 'master')
+    env.port = fab_settings.get('SSH_PORT', 22)
 
 
 ################ GIT ################
@@ -35,14 +39,13 @@ def push():
     """
     push and pull
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
-    # local("git submodule foreach git push")
-    local("git push origin master")
+    require('hosts')
+    require('venv_app')
+    # local('git submodule foreach git push')
+    local(f'git push origin {env.main_branch}')
     with prefix(env.venv_app):
-        run("git pull")
-        run("git submodule update --init --recursive")
-
+        run('git pull')
+        run('git submodule update --init --recursive')
 
 ################ DEPLOY ################
 
@@ -52,10 +55,11 @@ def pip():
     """
     install requirements
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
-        run("pip install -r requirements.txt")
+        run('pip install --upgrade pip')
+        run('pip install -r requirements.txt')
 
 
 @task
@@ -63,10 +67,10 @@ def collectstatic():
     """
     collect static files
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
-        run("python manage.py collectstatic --noinput")
+        run('python manage.py collectstatic --noinput')
 
 
 @task
@@ -74,8 +78,8 @@ def migrate():
     """
     execute migrations
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
         migrate_apps = fab_settings['MIGRATE_APPS']
         if not migrate_apps: migrate_apps = ''
@@ -87,10 +91,11 @@ def requirements():
     """
     install pip requirements
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
-        run("pip install -r requirements.txt")
+        run('pip install --upgrade pip')
+        run('pip install -r requirements.txt')
 
 
 @task
@@ -98,10 +103,10 @@ def upgrade_pip():
     """
     upgrade pip
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     with prefix(env.venv_app):
-        run("pip install --upgrade pip")
+        run('pip install --upgrade pip')
 
 
 @task
@@ -109,8 +114,8 @@ def restart():
     """
     Restart apache on the server.
     """
-    require('hosts', provided_by=[prod])
-    require('restart_script', provided_by=[prod])
+    require('hosts')
+    require('restart_script')
     with cd(env.webapp_dir):
         run(env.restart_script)
 
@@ -120,16 +125,16 @@ def deploy():
     """
     push, pull, collect static, restart
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     push()
     deploy_django()
 
 
 @task
 def deploy_django():
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     collectstatic()
     migrate()
     restart()
@@ -139,7 +144,7 @@ def deploy_django():
 
 
 @task
-def media_sync():
+def sync_media():
     """
     Download production media files to local computer
     """
@@ -152,11 +157,13 @@ def db_dump():
     """
     dump entire db on server and retrieve it
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
+    dump_data_models = fab_settings['DUMP_DATA_MODELS']
+
     with prefix(env.venv_app):
         run('mkdir -p data')
-        run(f'./manage.py dumpdata {fab_settings["DUMP_DATA_MODELS"]} --indent=4 > data/db.json')
+        run(f'./manage.py dumpdata {dump_data_models} --indent=2 > data/db.json')
         run('tar cvfz data/db.tgz data/db.json')
         run('rm data/db.json')
     local('mkdir -p data')
@@ -182,11 +189,12 @@ def initial_dump():
     """
     dump initial data on server and retrieve it
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
-    dump_initial_models = fab_settings["DUMP_INITIAL"]
+    require('hosts')
+    require('venv_app')
+    dump_initial_models = fab_settings['DUMP_INITIAL']
+
     with prefix(env.venv_app):
-        run(f'./manage.py dumpdata {dump_initial_models} --indent=4 > data/initial.json')
+        run(f'./manage.py dumpdata {dump_initial_models} --indent=2 > data/initial.json')
     local(f'scp {get_app_ssh_path()}/data/initial.json data')
 
 
@@ -196,6 +204,8 @@ def initial_load():
     load initial data on local computer
     """
     local('./manage.py loaddata data/initial.json')
+    if os.path.exists('data/initial_local.json'):
+        local('./manage.py loaddata data/initial_local.json')
 
 
 @task
@@ -205,7 +215,7 @@ def reset_local_db():
     """
     username = fab_settings['SUPERUSER_NAME']
     email = fab_settings['SUPERUSER_MAIL']
-    local('rm db.sqlite3')
+    local('rm -f db.sqlite3')
     local('./manage.py migrate')
     print('\n\n\nenter admin password:\n\n\n')
     local(f'./manage.py createsuperuser --username {username} --email {email}')
@@ -220,9 +230,19 @@ def hourly():
     """
     run hourly jobs, including submit mails
     """
-    require('venv_app', provided_by=[prod])
+    require('venv_app')
     with prefix(env.venv_app):
-        run("python manage.py runjobs hourly")
+        run('python manage.py runjobs hourly')
+
+
+@task
+def daily():
+    """
+    run daily jobs
+    """
+    require('venv_app')
+    with prefix(env.venv_app):
+        run('python manage.py runjobs daily')
 
 
 ################ CELERY ################
@@ -233,14 +253,14 @@ def celery():
     """
     restarts celery
     """
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
+    require('hosts')
+    require('venv_app')
     run('monit restart celery')
 
 
 @task
 def monit():
-    require('hosts', provided_by=[prod])
-    require('venv_app', provided_by=[prod])
-    run("monit")
-    run("monit status")
+    require('hosts')
+    require('venv_app')
+    run('monit')
+    run('monit status')
