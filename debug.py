50abbfaebbd0bb263935d3d537a1766fbae37804
Martin Massera <martinmassera@gmail.com>
2024-08-09 12:40:53 -0300

    preventing circular imports

--- debug.py
+++ debug.py
@@ -1,11 +1,9 @@
-from django.urls import path, include
-
-
 def configure_debug_toolbar(installed_apps, middleware):
     installed_apps.append('debug_toolbar')
     middleware.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
 
 def add_debug_toolbar_urls(urlpatterns):
+    from django.urls import path, include  # importing here to prevent circular imports
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
