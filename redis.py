535f841e43bf6049f9b290d29a47ac45db029b9d
Martin Massera <martinmassera@gmail.com>
2024-09-18 16:42:34 -0300

    redis_lock

--- redis.py
+++ redis.py
@@ -1,6 +1,12 @@
 from redis import Redis
 from django.conf import settings
+from redlock.lock import RedLock, RedLockFactory
 
 
 def get_redis():
     return Redis(port=settings.REDIS_PORT)
+
+
+def redis_lock(lock_name, *args, **kwargs):
+    redis_connection = [{'host': 'localhost', 'port': settings.REDIS_PORT}]
+    return RedLock(lock_name, redis_connection, *args, **kwargs)
