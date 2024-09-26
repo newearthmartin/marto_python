d8d654cb981461e6b90046f60e2f565367fe2c66
Martin Massera <martinmassera@gmail.com>
2024-09-26 13:46:21 -0300

    quick read and write

--- files.py
+++ files.py
@@ -1,4 +1,6 @@
 import os
+import json
+import pickle
 from random import randint
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
@@ -59,3 +61,23 @@ def read_static_file(path):
 def get_extension(filename):
     ext = filename.rsplit('.', maxsplit=1)
     return ext[1].lower() if len(ext) == 2 else None
+
+
+def quick_write_json(filename, data):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(data))
+
+
+def quick_write_bin(filename, data):
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def quick_read_json(filename):
+    with open(filename) as f:
+        return json.loads(f.read())
+
+
+def quick_read_bin(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
