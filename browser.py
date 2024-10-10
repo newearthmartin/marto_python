32ca2931057bec3b4d627603407020e36d56bfb6
Martin Massera <martinmassera@gmail.com>
2024-10-10 13:57:47 -0300

    can open chromium instance or connect via CDP

--- browser.py
+++ browser.py
@@ -1,44 +1,17 @@
 import logging
 import atexit
-from django.conf import Settings
+from django.conf import settings
 from playwright.sync_api import sync_playwright
 from playwright._impl._errors import TargetClosedError
 
 logger = logging.getLogger(__name__)
 
-__playwright = None
-__browser = None
 
-
-def __get_playwright():
-    global __playwright
-    if not __playwright:
-        atexit.register(__close)
-        __playwright = sync_playwright().start()
-    return __playwright
-
-
-def __get_browser():
+def get_browser():
     global __browser
-    if __browser and not __browser.is_connected():
-        logger.warning('Browser is not connected')
-        __browser = None
     if not __browser:
-        executable_path = getattr(Settings, 'CHROMIUM_PATH', None)
-        __browser = __get_playwright().chromium.launch(headless=True, executable_path=executable_path)
+        __browser = __get_browser()
     return __browser
-
-
-def __close():
-    if __browser: __browser.close()
-
-
-def __get_browser_page(page_url, wait_load=True):
-    page = __get_browser().new_page()
-    page.goto(page_url)
-    if wait_load:
-        page.wait_for_load_state('load')
-    return page
 
 
 def get_browser_page(page_url, wait_load=True):
@@ -49,3 +22,36 @@ def get_browser_page(page_url, wait_load=True):
         global __browser
         __browser = None
         return __get_browser_page(page_url, wait_load=wait_load)
+
+
+__playwright = None
+__browser = None
+
+
+def __get_playwright():
+    global __playwright
+    if not __playwright:
+        def close():
+            if __browser: __browser.close()
+        atexit.register(close)
+        __playwright = sync_playwright().start()
+    return __playwright
+
+
+def __get_browser():
+    chromium_cdp = getattr(settings, 'CHROMIUM_CDP', None)
+    if chromium_cdp:
+        logger.info(f'Connecting to Chromium on {chromium_cdp}')
+        return __get_playwright().chromium.connect_over_cdp(chromium_cdp)
+    else:
+        chromium_path = getattr(settings, 'CHROMIUM_PATH', None)
+        logger.info('Creating Chromium instance' + (f' - path: {chromium_path}' if chromium_path else ''))
+        return __get_playwright().chromium.launch(headless=True, executable_path=chromium_path)
+
+
+def __get_browser_page(page_url, wait_load=True):
+    page = get_browser().new_page()
+    page.goto(page_url)
+    if wait_load:
+        page.wait_for_load_state('load')
+    return page
