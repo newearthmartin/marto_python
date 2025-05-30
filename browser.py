611d88fd8c64c3a4bba78d7c98482d07fb974124
Martin Massera <martinmassera@gmail.com>
2025-05-30 01:44:46 -0300

    useful browser methods

--- browser.py
+++ browser.py
@@ -8,26 +8,57 @@ logger = logging.getLogger(__name__)
 
 
 def run_on_page(page_url, func, logger_extra=None):
-    run = lambda: __run_on_page(page_url, func, logger_extra=logger_extra)
+    return run_catching_errors(lambda: __run_on_page(page_url, func, logger_extra=logger_extra),
+                               logger_extra=logger_extra)
 
+
+def __run_on_page(page_url, func, logger_extra=None):
+    with sync_playwright() as p:
+        with get_chromium(p) as browser:
+            logger.info(f'Opening page - {page_url}', extra=logger_extra)
+            with browser.new_page() as page:
+                response = open_page(page, page_url)
+                if response.status != 200: return None
+                page.wait_for_load_state('load')
+                return func(page)
+
+
+def get_chromium(p, logger_extra=None):
+    chromium_cdp = getattr(settings, 'CHROMIUM_CDP', None)
+    if chromium_cdp:
+        logger.debug(f'Connecting to Chromium on {chromium_cdp}', extra=logger_extra)
+        return p.chromium.connect_over_cdp(chromium_cdp)
+    else:
+        chromium_path = getattr(settings, 'CHROMIUM_PATH', None)
+        logger.debug(f'Creating Chromium instance - path: {chromium_path}', extra=logger_extra)
+        return p.chromium.launch(headless=True, executable_path=chromium_path)
+
+
+def open_page(page, url):
+    response = page.goto(url, timeout=getattr(settings, 'PLAYWRIGHT_TIMEOUT', None))
+    if response.status != 200:
+        logger.warning(f'HTTP status {response.status} on {url}')
+    return response
+
+def run_catching_errors(run_fn, logger_extra=None):
     try:
-        return run()
+        return run_fn()
     except playwright_errors.TargetClosedError:
         logger.warning('Browser closed! retrying once', extra=logger_extra)
-        return run()
+        return run_fn()
     except playwright_errors.TimeoutError:
-        logger.warning(f'Timeout on {page_url}', extra=logger_extra)
+        logger.warning(f'Timeout on page', extra=logger_extra)
         return None
     except playwright_errors.Error as e:
         if 'net::ERR_ABORTED' in e.message:
             logger.warning('Browser connection aborted! retrying once', extra=logger_extra)
-            return run()
+            return run_fn()
         elif 'ECONNREFUSED' in e.message:
             logger.warning('Browser connection refused! retrying once', extra=logger_extra)
-            return run()
+            return run_fn()
         elif 'connect_over_cdp' in e.message:
             logger.warning(f'Browser connection error! {e.message.split('\n')[0]} - retrying once', extra=logger_extra)
-            return run()
+            return run_fn()
         elif 'net::ERR_SSL_VERSION_OR_CIPHER_MISMATCH' in e.message:
             log_msg = e.message.split('Call log:')[0].strip()
             logger.warning(log_msg, extra=logger_extra)
@@ -37,28 +68,3 @@ def run_on_page(page_url, func, logger_extra=None):
             return None
         else:
             raise e
-
-
-def __run_on_page(page_url, func, logger_extra=None):
-    with sync_playwright() as p:
-        with __get_chromium(p) as browser:
-            logger.info(f'Opening page - {page_url}', extra=logger_extra)
-            with browser.new_page() as page:
-                timeout = getattr(settings, 'PLAYWRIGHT_TIMEOUT', None)
-                response = page.goto(page_url, timeout=timeout)
-                if response.status != 200:
-                    logger.warning(f'HTTP status {response.status} on {page_url}')
-                    return None
-                page.wait_for_load_state('load')
-                return func(page)
-
-
-def __get_chromium(p, logger_extra=None):
-    chromium_cdp = getattr(settings, 'CHROMIUM_CDP', None)
-    if chromium_cdp:
-        logger.debug(f'Connecting to Chromium on {chromium_cdp}', extra=logger_extra)
-        return p.chromium.connect_over_cdp(chromium_cdp)
-    else:
-        chromium_path = getattr(settings, 'CHROMIUM_PATH', None)
-        logger.debug(f'Creating Chromium instance - path: {chromium_path}', extra=logger_extra)
-        return p.chromium.launch(headless=True, executable_path=chromium_path)
