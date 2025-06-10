963f713dcd15c8ca65675cf7604db66099bb6a66
Martin Massera <martinmassera@gmail.com>
2025-06-10 12:16:29 -0300

    app_prefix

--- fabfile_django.py
+++ fabfile_django.py
@@ -11,12 +11,37 @@ def get_prod(c):
     global __prod
     if not __prod:
         config = Config(overrides={"run": {"echo": True}})
-        __prod = Connection(host=c.default.host, user=c.default.user, config=config)
+        host = c.default.host
+        user = getattr(c.default, "user", None)
+        port = getattr(c.default, "port", None)
+        __prod = Connection(host=host, user=user, port=port, config=config)
     return __prod
 
 
 def app_ssh_path(c):
-    return f'{c.settings.prod_server}:{c.settings.app_dir}/{c.settings.app_django_dir}'
+    app_dir = c.settings.app_dir
+    app_django_dir = getattr(c.settings, 'app_django_dir', None)
+    path = f'{app_dir}/{app_django_dir}' if app_django_dir else app_dir
+    return f'{c.settings.prod_server}:{path}'
+
+
+def app_prefix(c, conn):
+    prefix = getattr(c.settings, 'venv_script', None) or f'cd {c.settings.app_dir}'
+    return conn.prefix(prefix)
+
+
+def scp_cmd(c):
+    cmd = 'scp'
+    if port := getattr(c.default, 'port', None):
+        cmd += f' -P {port}'
+    return cmd
+
+
+def rsync_cmd(c):
+    cmd = 'rsync'
+    if port := getattr(c.default, 'port', None):
+        cmd += f' --port {port}'
+    return cmd
 
 
 # ############### GIT ################
@@ -39,9 +64,10 @@ def push(c):
     Push and pull
     """
     conn = get_prod(c)
+
     main_branch = getattr(c.settings, 'main_branch', 'main')
     conn.local(f'git push origin {main_branch}')
-    with conn.prefix(c.settings.venv_script):
+    with app_prefix(c, conn):
         conn.run('git pull')
         conn.run('git submodule update --init --recursive')
 
@@ -56,7 +82,7 @@ def pip(c):
     """
     pip_cmd = getattr(c.settings, 'pip', 'pip')
     conn = get_prod(c)
-    with conn.prefix(c.settings.venv_script):
+    with app_prefix(c, conn):
         conn.run(f'{pip_cmd} install --upgrade pip')
         conn.run(f'{pip_cmd} install -r requirements.txt')
 
@@ -67,7 +93,7 @@ def collect_static(c):
     Collect static files
     """
     conn = get_prod(c)
-    with conn.prefix(c.settings.venv_script):
+    with app_prefix(c, conn):
         conn.run('python manage.py collectstatic --noinput')
 
 
@@ -77,7 +103,7 @@ def migrate(c):
     Execute migrations
     """
     conn = get_prod(c)
-    with conn.prefix(c.settings.venv_script):
+    with app_prefix(c, conn):
         migrate_apps = c.settings.get('migrate_apps', None)
         conn.run('python manage.py migrate' + (f' {migrate_apps}' if migrate_apps else ''))
 
@@ -111,7 +137,8 @@ def sync_media(c):
     Download production media files to local computer
     """
     conn = get_prod(c)
-    conn.local(f'rsync -avz {app_ssh_path(c)}/media/ media/')
+    rsync = rsync_cmd(c)
+    conn.local(f'{rsync} -avz {app_ssh_path(c)}/media/ media/')
 
 
 @task
@@ -119,14 +146,16 @@ def db_dump(c):
     """
     Dump entire db on server and retrieve it
     """
+    models = getattr(c.settings, 'dump_data_models', None)
     conn = get_prod(c)
-    with conn.prefix(c.settings.venv_script):
+    with app_prefix(c, conn):
         conn.run('mkdir -p data', hide=True)
-        conn.run(f'./manage.py dumpdata {c.settings.dump_data_models} --indent=2 > data/db.json')
+        conn.run(f'./manage.py dumpdata {models or ''} --indent=2 > data/db.json')
         conn.run('tar cvfz data/db.tgz data/db.json')
         conn.run('rm data/db.json', hide=True)
     conn.local('mkdir -p data', hide=True)
-    conn.local(f'scp {app_ssh_path(c)}/data/db.tgz data')
+    scp = scp_cmd(c)
+    conn.local(f'{scp} {app_ssh_path(c)}/data/db.tgz data')
 
 
 @task
@@ -150,9 +179,11 @@ def initial_dump(c):
     Dump initial data on server and retrieve it
     """
     conn = get_prod(c)
-    with conn.prefix(c.settings.venv_script):
+    with app_prefix(c, conn):
         conn.run(f'./manage.py dumpdata {c.settings.dump_initial} --indent=2 > data/initial.json')
-    conn.local(f'scp {app_ssh_path(c)}/data/initial.json data')
+
+    scp = scp_cmd(c)
+    conn.local(f'{scp} {app_ssh_path(c)}/data/initial.json data')
 
 
 @task
@@ -169,7 +200,7 @@ def initial_load(c):
 @task
 def reset_local_db(c):
     """
-    resets local db
+    Resets local db
     """
     user = c.settings.superuser_user
     email = c.settings.superuser_mail
@@ -186,7 +217,7 @@ def reset_local_db(c):
 
 def __run_jobs(c, job_type):
     conn = get_prod(c)
-    with conn.prefix(c.settings.venv_script):
+    with app_prefix(c, conn):
         conn.run(f'python manage.py runjobs {job_type}')
 
 
