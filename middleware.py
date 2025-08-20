549eb880e9b73cd0da7bffb2af3715b1d000e093
Martin Massera <martinmassera@gmail.com>
2025-08-20 15:13:01 -0300

    is_exempt in MaintenanceMiddleware

--- middleware.py
+++ middleware.py
@@ -1,6 +1,7 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.conf import settings
 
 
 class ViewShortcutMiddleware(MiddlewareMixin):
@@ -16,7 +17,14 @@ class ViewShortcutMiddleware(MiddlewareMixin):
 
 # noinspection PyMethodMayBeStatic
 class MaintenanceMiddleware(MiddlewareMixin):
+    def is_exempt(self, request, view_fn, view_args, view_kwargs):
+        if request.path.startswith('/admin/'): return True
+        if (static_url := getattr(settings, 'STATIC_URL', None)) and request.path.startswith(static_url): return True
+        if (media_url := getattr(settings, 'MEDIA_URL', None)) and request.path.startswith(media_url): return True
+        return False
+
     def process_view(self, request, view_func, view_args, view_kwargs):
+        if self.is_exempt(request, view_func, view_args, view_kwargs): return None
         template = loader.get_template('maintenance.html')
         request_context = RequestContext(request, {}, processors=[])
         return HttpResponse(template.template.render(request_context), status=503)
