6faf3f8de371ae187623668e57537511bfd29f99
Martin Massera <martinmassera@gmail.com>
2025-10-17 15:25:58 -0300

    apps.py - defaulting to AutoField

---
+++ marto_email/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MartoEmailAppConfig(AppConfig):
+    name = "marto_email"
+    verbose_name = "Marto Email"
+    default_auto_field = "django.db.models.AutoField"
