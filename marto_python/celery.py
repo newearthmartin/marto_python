dc555a9483079a3567fcb60c8d9aa10c69545e2f
Martin Massera <martinmassera@gmail.com>
2026-08-05 00:12:51 -0300

    Add task_only_one, track_task_loop and track_task_view for deduping and polling Celery tasks

--- marto_python/celery.py
+++ marto_python/celery.py
@@ -1,17 +1,82 @@
 import json
 import logging
+import time
 from datetime import datetime, timedelta
+from functools import wraps
 
-from celery import shared_task, signature
+from celery import shared_task, signature, current_task
+from celery.result import AsyncResult
 from django.utils import timezone
 from django.core.mail import EmailMessage
 from django.conf import settings
+from django.http import HttpResponseBadRequest, JsonResponse
 from threading import Thread
-from marto_python.redis import get_redis, get_signature_redis_key
+from redlock.lock import RedLockError
+from marto_python.redis import get_redis, get_redis_key, get_signature_redis_key, redis_lock
 from marto_python.strings import cut_str
 
 
 logger = logging.getLogger(__name__)
+
+
+MAX_TRACK_SECONDS = 10
+TASK_LOCK_TTL = 5 * 60 * 1000
+
+
+def track_task_view(request):
+    # TODO:
+    # This is a fast view method for getting the result of a task, but there is no security check
+    #  that the caller of this method is the one that submitted the task.
+    task_id = request.GET.get('task_id', None)
+    if not task_id:
+        return HttpResponseBadRequest()
+    response = track_task_loop(task_id)
+    return JsonResponse(response)
+
+
+def track_task_loop(task_id):
+    task = AsyncResult(task_id)
+    now = datetime.now()
+    while (datetime.now() - now).seconds < MAX_TRACK_SECONDS:
+        if task.successful():
+            return task.get()
+        elif task.failed():
+            return {'error': f'Error executing background task {task_id}'}
+        time.sleep(0.1)
+    return {'task_id': task_id}
+
+
+def task_only_one(*decorator_args, **decorator_kwargs):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            redis_key = 'only_one.' + get_redis_key(fn, *args, **kwargs)
+            task_id_key = f'{redis_key}.task_id'
+            try:
+                with redis_lock(redis_key, *decorator_args, **decorator_kwargs):
+                    redis = get_redis()
+                    task_id = current_task.request.id
+                    ttl = decorator_kwargs.get('ttl')
+                    if task_id:
+                        if ttl:
+                            redis.set(task_id_key, task_id, px=ttl)
+                        else:
+                            redis.set(task_id_key, task_id)
+                    try:
+                        return fn(*args, **kwargs)
+                    finally:
+                        if task_id:
+                            redis.delete(task_id_key)
+            except RedLockError:
+                logger.warning(f'Task {redis_key} already being executed.')
+                active_task_id = get_redis().get(task_id_key)
+                if active_task_id:
+                    if isinstance(active_task_id, bytes):
+                        active_task_id = active_task_id.decode()
+                    return {'task_id': active_task_id}
+                return {'task_already': True}
+        return wrapper
+    return decorator
 
 
 @shared_task
